a35df2934eb1a42048f5048bd8b4a295c0508896
Masami Ichikawa <masami256@gmail.com>
2019-05-09 11:16:25 +0900

    update logic

--- show-packages-info.py
+++ show-packages-info.py
@@ -11,7 +11,6 @@ def run_cmd(cmd):
     
     with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
         proc.wait()
-        
         retcode = proc.returncode
 
         for line in proc.stdout.readlines():
@@ -22,6 +21,55 @@ def run_cmd(cmd):
             "lines": lines
         }
     return None
+
+def find_packages_not_built_from_debian(packages, licenses, base_workdir):
+    cmd = ["find", base_workdir, "-maxdepth", "2", "-type", "d"]
+    
+    result = run_cmd(cmd)
+
+    package_dirs = result["lines"]
+    result = {}
+    
+    for package_dir in package_dirs:
+        
+        package_dir = package_dir.strip()
+        pkgname = os.path.basename(package_dir)
+        actual_pkgname = pkgname
+        if pkgname.endswith("-native"):
+            pkgname = pkgname.strip("-native")
+
+        if actual_pkgname in packages.keys():
+            if actual_pkgname in licenses:
+                lic = licenses[actual_pkgname]
+
+                cmd = ["find", package_dir, "-maxdepth", "2", "-name", "*.dsc"]
+                dsc_result = run_cmd(cmd)
+
+                if len(dsc_result["lines"]) is 0:
+                    result[actual_pkgname] = packages[actual_pkgname]
+                
+    return result
+                
+def get_bitbake_envs():
+    cmd = ["bitbake", "-e"]
+    lines = []
+
+    result = {}
+
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+        for line in proc.stdout.readlines():
+            l = line.decode('utf-8')
+    
+            if l.startswith("BASE_WORKDIR=") or \
+               l.startswith("TMPDIR="):
+                tmp  = l.strip().replace('"', '').split('=')
+                result[tmp[0]] = tmp[1]
+
+    if len(result) is 0:
+        print("Cannot get BASE_WORKDIR environment", file=sys.stderr)
+        exit(1)
+
+    return result
 
 def has_bitbake():
     cmd = ["which", "bitbake-layers"]
@@ -115,41 +163,19 @@ def get_recipes():
             
     return recipes
 
-def read_license_manifest(license_dir):
-
-    licenses = {}
+def get_packages_in_rootfs(license_dir):
+    packages = []
     path = "%s/license.manifest" % license_dir
+
     with open(path, "r") as f:
         lines = f.readlines()
-        length = len(lines)
 
-        i = 0
-        while True:
-            if i >= length:
-                break
-            
-            line = lines[i].strip()
+        for line in lines:
+            if line.startswith("PACKAGE NAME:"):
+                name = line.strip().split(':')[1].strip()
+                packages.append(name)
 
-            if len(line) > 0:
-                pkg_name = lines[i].strip().split(':')
-                pkg_version = lines[i + 1].strip().split(':')
-                pkg_recipe_name = lines[i + 2].strip().split(':')
-                pkg_license = lines[i + 3].strip().split(':')
-
-                lic_info = {
-                    "name": pkg_name[1].strip(),
-                    "version": pkg_version[1].strip(),
-                    "recipe": pkg_recipe_name[1].strip(),
-                    "pkg_license": pkg_license[1].strip()
-                }
-
-                licenses[lic_info["name"]] = lic_info
-                
-                i += 4
-            else:
-                i += 1           
-
-    return licenses
+    return packages
 
 def read_all_package_licenses(license_dir):
     licenses = {}
@@ -164,6 +190,7 @@ def read_all_package_licenses(license_dir):
                     lic_info = {
                         "name": dir,
                         "pkg_license": lines[0].split(':')[1].strip(),
+                        "pr": lines[1].split(':')[1].strip(),
                         "version": lines[2].split(':')[1].strip(),
                     }
 
@@ -223,38 +250,22 @@ def merge_data(layers, recipes, licenses):
 
     return data
 
-def is_target_layer(layer, target_layers):
-    return layer in target_layers
-        
-def create_verbose_data(data, target_layers):
-    result = {}
+def show_no_data():
+    print("All packages are built from debian source!")
 
-    for k in data.keys():
-        d = data[k]
-
-        if target_layers is None or is_target_layer(d["layer"], target_layers):
-            result[k] = d
-
-    return result
-
-def show_no_data(layers):
-    print("Packages not use %s layers" % ",".join(layers))
-
-def show_result(data, show_all, target_layers):
+def show_result(data):
     if len(data) == 0:
-        show_no_data(target_layers)
-        return
+        show_no_data()
+        return 0
     
-    if not show_all and target_layers is not None:
-        layers = ",".join(target_layers)    
-        print("===== Package in layer %s ====" % layers)
-    else:
-        print("==== Packages info ====")
+    print("==== Packages not built from debian source ====")
 
     print("%s\t%s\t%s\t%s\t%s" % ("package name".ljust(30), "layer".ljust(20), "recipe name".ljust(30), "version".ljust(10), "license".ljust(20)))
     for k in data.keys():
         d = data[k]
         print("%s\t%s\t%s\t%s\t%s" % (d["pkg_name"].ljust(30), d["layer"].ljust(20), d["recipe_name"].ljust(30), d["version"].ljust(10), d["license"].ljust(20)))
+
+    return 1
 
 def show_version():
     print("%s version 0.1" % os.path.basename(sys.argv[0]))
@@ -266,75 +277,54 @@ def license_dir_exists(path):
     
     return os.path.exists(path)
 
-def show_licence_dir_error(path):
-    print("licence directory [%s] is not found" % path, file=sys.stderr)
-    exit(1)
-    
 def parse_arguments():
-    usage = "usage: %prog [options] licence_directory"
-    
-    parser = OptionParser(usage=usage)
-
-    parser.add_option("-l", "--layers", dest="layer_names",
-                      help="comma separeted layer names",
-                      metavar="layers", default=None)
-    
-    parser.add_option("-v", "--version", dest="version",
-                      help="show program version",
-                      action="store_true", default=False)
-
-    parser.add_option("-a", "--all", dest="all_info",
-                      help="show all package info",
-                      action="store_true", default=False)
+    parser = OptionParser()
 
     parser.add_option("-r", "--rootfs", dest="rootfs",
-                       help="check package in rootfs",
-                       action="store_true", default=False)
+                       help="path to image directory(e.g. $BASE_WORKDIR/tmp/deploy/licenses/core-image-minimal-qemuarm64-20190507062402)",
+                       metavar="ROOTFS", default=None)
 
     (options, args) = parser.parse_args()
 
-    if len(args) == 0:
-        print("must specify license directory", file=sys.stderr)
-        exit(1)
-    
     return {
-        "license_directory": args[0],
-        "version": options.version,
-        "all": options.all_info,
-        "layer_names": options.layer_names,
         "rootfs": options.rootfs,
     }
 
 if __name__ == "__main__":
     args = parse_arguments()
-    
-    if args["version"]:
-        show_version()
 
-    if not license_dir_exists(args["license_directory"]):
-        show_licence_dir_error(args["license_directory"])
-        
     has_bitbake()
+
+    bitbake_envs = get_bitbake_envs()
+    
     layers = get_layers()
     recipes = get_recipes()
 
-    check_target_layers = None
-    if args["layer_names"]:
-        check_target_layers = args["layer_names"].split(',')
-        
-    licenses = None
+    license_dir = "%s/deploy/licenses" % bitbake_envs["TMPDIR"]
 
-    if args["rootfs"]:
-        licenses = read_license_manifest(args["license_directory"])
+    licenses = read_all_package_licenses(license_dir)
+    set_package_recipe(licenses, recipes)
+
+    all_packages = merge_data(layers, recipes, licenses)
+    packages = None
+
+    if not args["rootfs"]:
+        packages = all_packages
     else:
-        licenses = read_all_package_licenses(args["license_directory"])
-        set_package_recipe(licenses, recipes)
+        rootfs_packages = get_packages_in_rootfs(args["rootfs"])
+        packages = {}
+        for k in all_packages:
+            pkgname = all_packages[k]["pkg_name"]
+            if pkgname in rootfs_packages:
+                packages[k] = all_packages[k]
 
-    data = merge_data(layers, recipes, licenses)
-    
-    if not args["all"]:
-        data = create_verbose_data(data, check_target_layers)
 
-    show_result(data, args["all"], check_target_layers)
+    packages = find_packages_not_built_from_debian(packages, licenses, bitbake_envs["BASE_WORKDIR"])
 
-    exit(0)
+    r = 0
+    sorted_packages = {}
+    for k, v in sorted(packages.items(), key=lambda x: x[0]):
+        sorted_packages[k] = v
+    r = show_result(sorted_packages)
+
+    exit(r)
